d379dd05557a8d645840536ef82464ff6a36af53
Fernando Tanta <gestor_usi01@minedu.gob.pe>
2024-11-28 22:21:23 -0500

    semana06

---
+++ semana06/cursos/Cursos/src/Cursos/Cursos.Api/Program.cs
@@ -0,0 +1,27 @@
+using Cursos.Api.Extensions;
+using Cursos.Application;
+using Cursos.Infrastructure;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+
+builder.Services.AddApplication();
+builder.Services.AddInfraestructure(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCustomExceptionHandler();
+app.MapControllers();
+
+app.Run();
