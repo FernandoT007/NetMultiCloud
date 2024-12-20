2e1a8b25b6e455e0c628b79ec4c0a607e3c0f55b
Fernando Tanta <gestor_usi01@minedu.gob.pe>
2024-12-19 23:07:11 -0500

    semana09

---
+++ semana09/cursos/src/Cursos/Cursos.Api/Program.cs
@@ -0,0 +1,39 @@
+using Cursos.Api.Extensions;
+using Cursos.Api.gRPC;
+using Cursos.Application;
+using Cursos.Infrastructure;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddGrpc();
+builder.WebHost.ConfigureKestrel((context, options) =>
+{
+    options.Configure(context.Configuration.GetSection("Kestrel"));
+});
+
+builder.Services.AddGrpc();
+
+builder.Services.AddControllers();
+
+builder.Services.AddApplication();
+builder.Services.AddInfraestructure(builder.Configuration);
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCustomExceptionHandler();
+app.MapControllers();
+app.MapGrpcService<CursosGrpcService>();
+
+app.Run();
